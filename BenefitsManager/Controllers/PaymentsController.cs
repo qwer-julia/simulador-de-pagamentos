b8275963b3ad4bd47b3d6d02dfa328ee5d7794f9
Júlia Fonseca <79732591+qwer-julia@users.noreply.github.com>
2024-12-20 22:00:14 -0300

    Removendo salvar o pagamento no banco de dados

--- BenefitsManager/Controllers/PaymentsController.cs
+++ BenefitsManager/Controllers/PaymentsController.cs
@@ -25,33 +25,6 @@ namespace BenefitsManager.Controllers
         // GET: Payments
         public async Task<IActionResult> Index()
         {
-            var context = _context.Payments.Include(p => p.Benefit).Include(p => p.Taxpayer);
-            return View(await context.ToListAsync());
-        }
-
-        // GET: Payments/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var payment = await _context.Payments
-                .Include(p => p.Benefit)
-                .Include(p => p.Taxpayer)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (payment == null)
-            {
-                return NotFound();
-            }
-
-            return View(payment);
-        }
-
-        // GET: Payments/SearchTaxpayer
-        public IActionResult SearchTaxpayer()
-        {
             return View();
         }
 
@@ -62,16 +35,12 @@ namespace BenefitsManager.Controllers
         {
             var taxpayer = await _context.Taxpayers
                 .Include(t => t.TaxpayerBenefits)
-                .ThenInclude(tb => tb.Benefit)
                 .FirstOrDefaultAsync(t => t.Cnpj == cnpj);
-
-            // verificar necessidade de include
 
             if (taxpayer == null)
             {
-                Console.WriteLine("NAO ENCONTRADO");
                 TempData["Error"] = "CNPJ não encontrado. Verifique e tente novamente.";
-                return View();
+                return RedirectToAction("Index");
             }
 
             return RedirectToAction("Create", new { taxpayerId = taxpayer.Id });
@@ -109,33 +78,28 @@ namespace BenefitsManager.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(CreatePaymentViewModel model)
         {            
-            if (ModelState.IsValid)
+            var benefit = await _context.Benefits
+                .FirstOrDefaultAsync(m => m.Id == model.BenefitId);
+
+            var finalValue = model.InitialValue;
+
+            if (benefit != null)
             {
-                var benefit = await _context.Benefits
-                    .FirstOrDefaultAsync(m => m.Id == model.BenefitId);
-
-                var finalValue = model.InitialValue;
-
-                if (benefit != null)
-                {
-                    finalValue = model.InitialValue * (1 - (benefit.DiscountPercentage / 100));
-                }
-
-                var payment = new Payment
-                {
-                    TaxpayerId = model.TaxpayerId,
-                    BenefitId = model.BenefitId,
-                    InitialValue = model.InitialValue,
-                    FinalValue = finalValue,
-                    Benefit = benefit,
-                    Taxpayer = await _context.Taxpayers.FirstOrDefaultAsync(m => m.Id == model.TaxpayerId)
-                };
-
-                HttpContext.Session.SetString("PaymentData", JsonConvert.SerializeObject(payment));
-                return RedirectToAction("ConfirmPayment");
+                finalValue = model.InitialValue * (1 - (benefit.DiscountPercentage / 100));
             }
 
-            return View(model);
+            var payment = new Payment
+            {
+                TaxpayerId = model.TaxpayerId,
+                BenefitId = model.BenefitId,
+                InitialValue = model.InitialValue,
+                FinalValue = finalValue,
+                Benefit = benefit,
+                Taxpayer = await _context.Taxpayers.FirstOrDefaultAsync(m => m.Id == model.TaxpayerId)
+            };
+
+            HttpContext.Session.SetString("PaymentData", JsonConvert.SerializeObject(payment));
+            return RedirectToAction("ConfirmPayment");
         }
         public IActionResult ConfirmPayment()
         {
@@ -143,54 +107,6 @@ namespace BenefitsManager.Controllers
             var payment = JsonConvert.DeserializeObject<Payment>(paymentJson);
 
             return View(payment);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> SavePayment(Payment payment)
-        {
-            _context.Payments.Add(payment);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        // GET: Payments/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var payment = await _context.Payments
-                .Include(p => p.Benefit)
-                .Include(p => p.Taxpayer)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (payment == null)
-            {
-                return NotFound();
-            }
-
-            return View(payment);
-        }
-
-        // POST: Payments/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var payment = await _context.Payments.FindAsync(id);
-            if (payment != null)
-            {
-                _context.Payments.Remove(payment);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool PaymentExists(int id)
-        {
-            return _context.Payments.Any(e => e.Id == id);
         }
     }
 }
